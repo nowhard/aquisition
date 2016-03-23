c419634f5f362f2f6258426bdc0bc8e531bebebd
nowhard <nowhard@mail.ru>
2016-03-23 17:53:45 +0300

    Продолжение проектирования

--- modules/adc_read.v
+++ modules/adc_read.v
@@ -15,6 +15,14 @@ module adc_read #(parameter DATA_WIDTH = 24, DIAP_WIDTH = 2)(
 	parameter MEASURE_FOR_DIAP_PERIODS		=	2;
 	parameter MEASURE_FOR_RESULT_PERIODS	= 32;
 	parameter MEASURE_SAMPLES_IN_PERIOD		= 32;
+	parameter MEASURE_DIAP_SAMPLES			= MEASURE_FOR_DIAP_PERIODS*MEASURE_SAMPLES_IN_PERIOD;
+	parameter MEASURE_RESULT_SAMPLES			= MEASURE_FOR_RESULT_PERIODS*MEASURE_SAMPLES_IN_PERIOD;
+	
+	parameter MEASURE_RESULT_COUNT_WIDTH	= 10;
+	parameter MEASURE_DIAP_COUNT_WIDTH		=  6;
+	
+	parameter DIAP_INTEGRATOR_WIDTH			= 24;
+	parameter RESULT_INTEGRATOR_WIDTH		= 28;
 	
 
   localparam STATE_SIZE = 3;
@@ -27,14 +35,21 @@ module adc_read #(parameter DATA_WIDTH = 24, DIAP_WIDTH = 2)(
   reg [DATA_WIDTH-1:0] data_out_1_d, data_out_1_q;
   reg [DATA_WIDTH-1:0] data_out_2_d, data_out_2_q;
   reg [MEASURE_COUNT_WIDTH-1:0] measure_count_d, measure_count_q;
+  
+  reg sample_adc_last,sample_adc_next;
+  
+  reg [MEASURE_RESULT_COUNT_WIDTH-1:0]result_sample_counter;
+  reg [MEASURE_DIAP_COUNT_WIDTH-1:0]diap_sample_counter;
+  
+  reg[DIAP_INTEGRATOR_WIDTH-1:0] diap_integrator_d, diap_integrator_q;
+  reg[RESULT_INTEGRATOR_WIDTH-1:0] result_itegrator_d,result_itegrator_q;
    
 
- // assign busy = state_q != IDLE;
+  assign complete = state_q == IDLE;
   assign data_out_1 = data_out_1_q;
   assign data_out_2 = data_out_2_q;
   assign new_data = new_data_q;
-
-	
+  assign diap=diap_q;
 	
 	
 //FSM	one-hot state coding
@@ -47,9 +62,8 @@ module adc_read #(parameter DATA_WIDTH = 24, DIAP_WIDTH = 2)(
 				SHIFT_INTEGRATOR 	= 6'b100000,
 	
 
-	
-	// Determine the next state
-	always @ (posedge clk ) begin
+
+	always @ (*) begin	//FSM
 	 
 	 state_d=state_q;
 	 start_d=start_q;
@@ -59,57 +73,84 @@ module adc_read #(parameter DATA_WIDTH = 24, DIAP_WIDTH = 2)(
 	 data_out_2_d=data_out_2_q;
 	 measure_count_d=measure_count_q;
 	 	 
-			case (state_q)
+		case (state_q)
+		
+			IDLE:
+			begin
+				if(start_conv)
+				begin
+					state_d <= START_CONV;
+				end
+			end
 			
-				IDLE:
+			START_CONV:
+			begin
+				if ()
 				begin
-					if(start_conv)
-					begin
-						state_d <= START_CONV;
+					
+				end
+				else
+				begin
+					
+				end
+			end
+			
+			READ_FOR_DIAP:
+			begin
+				if ((sample_adc_last^sample_adc_next)&&(diap_sample_counter<MEASURE_DIAP_SAMPLES))
+				begin
+					
+				end
+				
+				else
+				
+				begin
+					state_d <= CALC_DIAP;
+				end
+			end
+			
+			CALC_DIAP:
+			begin
+				if ()
+				begin
+					
+				end
+				else
+				begin
+				
 					end
-				end
-				
-				START_CONV:
+			end
+			
+			READ_RESULT:
+			begin
+				if ()
 				begin
-					if ()
-						//state <= S2;
-					else
-						//state <= S1;
+					
 				end
-				
-				READ_FOR_DIAP:
-					if (data_in)
-						//state <= S3;
-					else
-						//state <= S1;
-				CALC_DIAP:
-					if ()
-						//state <= S2;
-					else
-						//state <= S3;		
-				READ_RESULT:
-					if ()
-						//state <= S2;
-					else
-						//state <= S3;
-				SHIFT_INTEGRATOR:
-					if ()
-						//state <= S2;
-					else
-						//state <= S3;						
-			endcase
+				else
+				begin
+					
+				end
+			end
+			
+			SHIFT_INTEGRATOR:
+			begin
+				if ()
+				begin
+					
+				end
+				else
+				begin
+
+				end
+			end
+			
+		endcase
 	end
 	
 	  always @(posedge clk) begin
     if (rst) begin
-   /*   ctr_q <= {BIT_CNT_WIDTH{1'b0}};
-      data_q <= {DATA_WIDTH{1'b0}};
-      sck_q <= 4'b0;
-      mosi_q <= 1'b0;
-      state_q <= IDLE;
-      data_out_q <= {DATA_WIDTH{1'b0}};
-      new_data_q <= 1'b0;*/
-		
+
 		 state_q<=IDLE;
 		 start_q<=1'b0;
 		 diap_q<=;
@@ -117,6 +158,12 @@ module adc_read #(parameter DATA_WIDTH = 24, DIAP_WIDTH = 2)(
 		 data_out_1_q<={DATA_WIDTH{1'b0}};
 		 data_out_2_q<={DATA_WIDTH{1'b0}};
 		 measure_count_q<={MEASURE_COUNT_WIDTH{1'b0}};
+		 diap_integrator_q<={DIAP_INTEGRATOR_WIDTH{1'b0}};
+		 result_integrator_q<={RESULT_INTEGRATOR_WIDTH{1'b0}};
+		 sample_adc_last<=1'b0;
+		 sample_adc_next<=1'b0;
+		 
+		 
     end else begin
 		 state_q<=state_d;
 		 start_q<=start_d;
@@ -125,6 +172,12 @@ module adc_read #(parameter DATA_WIDTH = 24, DIAP_WIDTH = 2)(
 		 data_out_1_q<=data_out_1_d;
 		 data_out_2_q<=data_out_2_d;
 		 measure_count_q<=measure_count_d;
+		 
+		 diap_integrator_q<=diap_integrator_d;
+		 result_integrator_q<=result_integrator_d;
+		 
+		 sample_adc_last<=sample_adc_next;
+		 sample_adc_next<=sample_adc;
     end
   
   endmodule
